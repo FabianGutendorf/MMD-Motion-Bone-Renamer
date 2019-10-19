0b7ded6d2116f0335069a88ad3fd2137d43044d7
Fabian Gutendorf <fgutendorf@aol.com>
2019-10-20 00:44:56 +0200

    Initial VS Studio Commit

---
+++ MMD Motion Bones Renamer/MMD Motion Bones Renamer/Main Window.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace MMD_Motion_Bones_Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenMotionData = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // OpenMotionData
+            // 
+            this.OpenMotionData.FileName = "Open Motion Data";
+            this.OpenMotionData.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 629);
+            this.Name = "Form1";
+            this.Text = "MMD Motion Bone Renamer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenMotionData;
+    }
+}
+
